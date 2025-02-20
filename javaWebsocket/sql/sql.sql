6ee3c096fc7685f6359ea27b7b42a19fc110d448
elcanbag <elcanbagirov09@gmail.com>
2025-02-20 09:14:10 +0400

    sql

---
+++ javaWebsocket/sql/sql.sql
@@ -0,0 +1,13 @@
+CREATE TABLE dates (
+    id SERIAL PRIMARY KEY,
+    temperature VARCHAR(50),
+    humidity VARCHAR(50),
+    x VARCHAR(50),
+    y VARCHAR(50),
+    z VARCHAR(50),
+    lat VARCHAR(50),
+    longg VARCHAR(50),
+    internal_temp VARCHAR(50),
+    pressure VARCHAR(50),
+    distance VARCHAR(50)
+);
